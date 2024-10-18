775e1395313cc76f7d8f8ddfd4d362a4a553bbb0
woody6624 <woody6624@gmail.com>
2024-10-19 00:28:08 +0900

    헬스체크 적용

--- src/main/java/com/dream/catcher/CatcherApplication.java
+++ src/main/java/com/dream/catcher/CatcherApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.dream.catcher")
 @EnableJpaAuditing
 @EnableScheduling
 @EnableJpaRepositories
