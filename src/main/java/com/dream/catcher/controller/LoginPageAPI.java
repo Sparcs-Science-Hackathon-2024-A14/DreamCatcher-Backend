48040e904466d6743450e88a7d29fc5e1c360958
D15M4S <122260287+D15M4S@users.noreply.github.com>
2024-10-18 19:18:55 +0900

    feat: BadgePage 수정

--- src/main/java/com/dream/catcher/controller/LoginPageAPI.java
+++ src/main/java/com/dream/catcher/controller/LoginPageAPI.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
-@RestController
+@RestController("api/")
 @RequiredArgsConstructor
 public class LoginPageAPI {
 
