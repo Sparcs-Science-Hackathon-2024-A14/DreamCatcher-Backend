1170cf23fa2340eb99e90eb706e04c448e4fef57
woody6624 <woody6624@gmail.com>
2024-10-18 21:02:25 +0900

    redis와의 거리계산 및 swagger적용

--- src/main/java/com/dream/catcher/controller/LoginPageAPI.java
+++ src/main/java/com/dream/catcher/controller/LoginPageAPI.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/login")
 @RequiredArgsConstructor
+@RequestMapping("/api/login")
 public class LoginPageAPI {
 
     private final LoginService loginService;
--- src/main/java/com/dream/catcher/controller/MapPageAPI.java
+++ src/main/java/com/dream/catcher/controller/MapPageAPI.java
@@ -1,10 +1,12 @@
 package com.dream.catcher.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/mainpage")
 public class MapPageAPI {
 
 }
