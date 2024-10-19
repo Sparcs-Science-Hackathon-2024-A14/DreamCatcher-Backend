570fd338f53444f2cfa17b62acddf430fe70a848
woody6624 <woody6624@gmail.com>
2024-10-19 14:13:32 +0900

    spot에 직렬화 적용

--- src/main/java/com/dream/catcher/config/RedisConfig.java
+++ src/main/java/com/dream/catcher/config/RedisConfig.java
@@ -7,6 +7,8 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -14,14 +16,18 @@ public class RedisConfig {
     public LettuceConnectionFactory connectionFactory() {
         return new LettuceConnectionFactory();
     }
-
     @Bean
-    public RedisTemplate<Long, SpotPositionDto> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<Long, SpotPositionDto> redisTemplate() {
         RedisTemplate<Long, SpotPositionDto> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
+        template.setConnectionFactory(connectionFactory());
 
-        // JSON 직렬화 설정
+        // Key와 Value의 직렬화 방식을 설정
+        RedisSerializer<String> stringSerializer = new StringRedisSerializer();
+        template.setKeySerializer(stringSerializer);
+        template.setHashKeySerializer(stringSerializer);
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
         return template;
     }
 }
