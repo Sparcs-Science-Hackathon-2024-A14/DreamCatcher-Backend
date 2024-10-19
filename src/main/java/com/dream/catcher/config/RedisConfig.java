afcd7e54ade9150a0951d06ec22749ff79823b83
woody6624 <woody6624@gmail.com>
2024-10-19 15:46:37 +0900

    spot에 직렬화 적용-> 키 타입 string으로 변경

--- src/main/java/com/dream/catcher/config/RedisConfig.java
+++ src/main/java/com/dream/catcher/config/RedisConfig.java
@@ -3,7 +3,7 @@ package com.dream.catcher.config;
 import com.dream.catcher.dto.SpotPositionDto;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -12,15 +12,25 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    private String hostName = "redis";
+
+    private int port = 6379;
+
     @Bean
-    public LettuceConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory();
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        // ! You have to provide the redisStandaloneConfiguration or else the app wont
+        // ! work with docker
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(hostName);
+        redisStandaloneConfiguration.setPort(port);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
+
 
     @Bean
     public RedisTemplate<String, SpotPositionDto> redisTemplate() {
         RedisTemplate<String, SpotPositionDto> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory());
+        template.setConnectionFactory(lettuceConnectionFactory());
 
         // Key와 Value의 직렬화 방식을 설정
         RedisSerializer<String> stringSerializer = new StringRedisSerializer();
