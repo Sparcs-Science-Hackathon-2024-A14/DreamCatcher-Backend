8fac44b8f90ac0b1be22bd0d4f8918d4befe3e1e
woody6624 <woody6624@gmail.com>
2024-10-19 12:51:43 +0900

    스웨거 수정

--- src/main/java/com/dream/catcher/config/SwaggerConfig.java
+++ src/main/java/com/dream/catcher/config/SwaggerConfig.java
@@ -1,39 +1,25 @@
 package com.dream.catcher.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import java.util.List;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@OpenAPIDefinition(
-        info = @Info(
-                title = "index 페이지에 들어갈 제목",
-                description = "index 페이지에 들어갈 내용"
-        )
-)
+@Configuration    // 스프링 실행시 설정파일 읽어드리기 위한 어노테이션
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI openAPI() {
-        SecurityScheme securityScheme = getSecurityScheme();
-        SecurityRequirement securityRequirement = getSecurityRequireMent();
-
         return new OpenAPI()
-                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
-                .security(List.of(securityRequirement));
+                .components(new Components())
+                .info(apiInfo());
     }
 
-    private SecurityScheme getSecurityScheme() {
-        return new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
-                .in(SecurityScheme.In.HEADER).name("Authorization");
-    }
-
-    private SecurityRequirement getSecurityRequireMent() {
-        return new SecurityRequirement().addList("bearerAuth");
+    private Info apiInfo() {
+        return new Info()
+                .title("DreamCathcer")
+                .description("Sparcs 해커톤 2024 - API 명세서")
+                .version("1.0.0");
     }
 }
