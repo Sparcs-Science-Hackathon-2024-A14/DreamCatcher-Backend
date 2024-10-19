5da1fcd63707fb6bd1c0821529d88e7ab44ffa69
woody6624 <woody6624@gmail.com>
2024-10-19 13:16:46 +0900

    cors설정 수정

--- src/main/java/com/dream/catcher/config/WebConfig.java
+++ src/main/java/com/dream/catcher/config/WebConfig.java
@@ -6,13 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("*") // 모든 출처 허용. 특정 출처만 허용하려면 예: "http://example.com"으로 수정
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메소드
-                .allowedHeaders("*") // 모든 헤더 허용
-                .allowCredentials(true); // 자격 증명 사용 허용
+        registry.addMapping("/**") // 모든 경로에 대해 CORS를 허용
+                .allowedOrigins("https://dream-catcher-hiha.shop") // 특정 도메인 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+                .allowCredentials(true) // 자격 증명 허용
+                .allowedHeaders("*"); // 모든 헤더 허용
     }
 }
