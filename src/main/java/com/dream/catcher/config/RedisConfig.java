dd442e036f7da936b0a6cb2d406c99b4cfa75752
woody6624 <woody6624@gmail.com>
2024-10-19 14:08:01 +0900

    spot에 직렬화 적용

--- src/main/java/com/dream/catcher/config/RedisConfig.java
+++ src/main/java/com/dream/catcher/config/RedisConfig.java
@@ -3,8 +3,10 @@ package com.dream.catcher.config;
 import com.dream.catcher.dto.SpotPositionDto;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -14,9 +16,12 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<Long, SpotPositionDto> redisTemplate() {
+    public RedisTemplate<Long, SpotPositionDto> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<Long, SpotPositionDto> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory());
+        template.setConnectionFactory(connectionFactory);
+
+        // JSON 직렬화 설정
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
     }
 }
