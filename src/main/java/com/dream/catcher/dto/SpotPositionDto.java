483e3f88d79553ec4941b86a80233576399476a4
woody6624 <woody6624@gmail.com>
2024-10-19 14:02:23 +0900

    spot에 직렬화 적용

--- src/main/java/com/dream/catcher/dto/SpotPositionDto.java
+++ src/main/java/com/dream/catcher/dto/SpotPositionDto.java
@@ -1,11 +1,22 @@
 package com.dream.catcher.dto;
 
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.jackson.Jacksonized;
 
-@Data
-@RequiredArgsConstructor
-public class SpotPositionDto {
+@SuperBuilder
+@Jacksonized
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class SpotPositionDto implements Serializable {
     private Long id; // Spot의 id
     private Double posX; // x좌표 -> 경도
     private Double posY; // y좌표 -> 위도
